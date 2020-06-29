6ba3a353b25fa44dae1e50d89654d3b1c7528dc7
SagrikaBhandari <65361122+SagrikaBhandari@users.noreply.github.com>
2020-06-29 22:00:20 +0530

    Update request.py

--- request.py
+++ request.py
@@ -2,6 +2,6 @@
 import requests
 
 url = 'http://localhost:5000/predict_api'
-r = requests.post(url,json={'SeniorCitizen':0,'Tenure':1, 'MonthlyCharges':29.85, 'TotalCharges':29.85})
+r = requests.post(url,json={'Age':44,'Gender':1, 'Total_Billirubin':8,"Aspartate_Aminotransferase":68, 'Total_Protiens':9})
 
 print(r.json())
