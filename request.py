1fc4bcec98d3573d408c8aeee6183680c363b51a
SagrikaBhandari <65361122+SagrikaBhandari@users.noreply.github.com>
2020-07-01 13:36:55 +0530

    Delete request.py

--- request.py
+++
@@ -1,7 +0,0 @@
-
-import requests
-
-url = 'http://localhost:5000/predict_api'
-r = requests.post(url,json={'Age':44,'Gender':1, 'Total_Billirubin':8,"Aspartate_Aminotransferase":68, 'Total_Protiens':9})
-
-print(r.json())
